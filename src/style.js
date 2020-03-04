f66b1461cb280ad8721df1fa47097173b8bdd0b8
Antihrist999 <R.a.v_36@mail.ru>
2020-03-04 18:16:25 +0300

    test

---
+++ src/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 500px;
+    border-style: solid;
+    border-color:red; 
+`
+
+export const Margin = styled.div`
+    padding-bottom: 20px;
+    border-color: silver;
+    border-style: solid;
+`
